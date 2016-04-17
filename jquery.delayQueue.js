15a1f96b8f1c0cf7681ba6bc739fd832f97df625
hectorqin <1069315972@qq.com>
2016-04-17 16:17:30 +0800

    Update jquery.delayQueue.js

--- jquery.delayQueue.js
+++ jquery.delayQueue.js
@@ -15,9 +15,9 @@
 			return false;
 		}
 		if(typeof($.timer)=="undefined"){
-            console.error('需要juery.timer.js库');
-            return false;
-        }
+	            console.error('需要juery.timer.js库');
+	            return false;
+	        }
         
 		var options = jQuery.extend({ locally : 1}, options);
 
